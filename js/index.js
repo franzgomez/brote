a8c0794281942d2c3f8d1ce3725955253c93c538
Francisco Gomez <fjg483@gmail.com>
2021-01-02 12:16:22 -0300

    github desktop

---
+++ js/index.js
@@ -0,0 +1,33 @@
+
+// <!--POPER sobre dama y niños-->
+
+
+
+   $(function(){
+       $("[data-toggle='popover']").popover();
+   });
+   
+
+
+
+// <!--TOOLTIPS sobre Hombres y damas-->
+
+
+
+   $(function(){
+       $("[data-toggle='tooltip']").tooltip();
+       });
+       
+
+
+
+// <!--CAROUSEL-->
+
+
+
+   $(function(){
+       $('.carousel').carousel({
+           interval: 2000
+       });
+       });
+
